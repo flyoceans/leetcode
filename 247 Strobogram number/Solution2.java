4e2aa6c78e6aa7fac9ca1a28b603549af5296738
flyoceans <pengyq2009@gmail.com>
2018-07-23 22:32:00 +0800

    July 23

--- 247 Strobogram number/Solution2.java
+++ 247 Strobogram number/Solution2.java
@@ -1,53 +1,32 @@
-class Solution {
-    public List<String> findStrobogrammatic(int n) {
-        if (n == 1)
-            return Arrays.asList("0","1","8");
-        List<String> ans = new ArrayList<>();
-        char[] dict = {'0', '1', '8', '6', '9'};
-        helper(ans, dict, new StringBuilder(), n);
-        full(ans, n);
-        return ans;
+public List<String> findStrobogrammatic(int n) {
+    findStrobogrammaticHelper(new char[n], 0, n - 1);
+    return res;
+}
+
+List<String> res = new ArrayList<String>();
+
+public void findStrobogrammaticHelper(char[] a, int l, int r) {
+    if (l > r) {
+        res.add(new String(a));
+        return;
     }
-    
-    private void helper(List<String> ans, char[] dict, StringBuilder sb, int n) {
-        if (sb.length() == (n+1)/2) {
-            ans.add(sb.toString());
-            return;
-        }
-        
-        for (int i = 0; i < dict.length; i++) {
-            if (sb.length() == 0 && i == 0)
-                continue;
-            if (sb.length() == n/2 && (i == 4 || i == 3) && n % 2 == 1)
-                continue;
-            sb.append(dict[i]);
-            helper(ans, dict, sb, n);
-            sb.deleteCharAt(sb.length()-1);
-        }
+    if (l == r) {
+        a[l] = '0'; res.add(new String(a));
+        a[l] = '1'; res.add(new String(a));
+        a[l] = '8'; res.add(new String(a));
         return;
     }
     
-    private void full(List<String> ans, int n) {
-        int len = (n % 2 == 0) ? ans.get(0).length() : ans.get(0).length()-1;
-
-        for (int i = 0; i < ans.size(); i++) {
-            String str = ans.get(i);
-            String rts = reverse(str, len);
-            ans.set(i, str + rts);
-        }
+    if (l != 0) {
+        a[l] = '0'; a[r] = '0';
+        findStrobogrammaticHelper(a, l+1, r-1);
     }
-    
-    private String reverse(String s, int len) {
-        String l = "";
-        while (len > 0) {
-            if (s.charAt(len-1) == '0' || s.charAt(len-1) == '1' || s.charAt(len-1) == '8') {
-                l += s.charAt(len-1);
-            } else {
-                if (s.charAt(len-1) == '6') l += '9';
-                else l += '6';
-            }
-            len--;
-        }
-        return l;
-    }
+    a[l] = '1'; a[r] = '1';
+    findStrobogrammaticHelper(a, l+1, r-1);
+    a[l] = '8'; a[r] = '8';
+    findStrobogrammaticHelper(a, l+1, r-1);
+    a[l] = '6'; a[r] = '9';
+    findStrobogrammaticHelper(a, l+1, r-1);
+    a[l] = '9'; a[r] = '6';
+    findStrobogrammaticHelper(a, l+1, r-1);
 }
