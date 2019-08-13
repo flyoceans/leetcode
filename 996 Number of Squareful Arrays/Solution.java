518a09e0ac6a24a9ed3b88bf54989da5ed5f8cea
flyoceans <pengyq2009@gmail.com>
2019-08-13 14:38:55 -0700

    8.13

---
+++ 996 Number of Squareful Arrays/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int numSquarefulPerms(int[] A) {
+        boolean visited[] = new boolean[A.length];
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(A);
+        helper(result, A, new ArrayList<>(), -1, visited);
+        return result.size();
+    }
+    
+    public void helper(List<List<Integer>> result, int A[], List<Integer> path, int last_number, boolean visited[]) {
+        if(path.size() == A.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for(int i = 0; i < A.length; i++) {
+            if(visited[i] || (i > 0 && A[i] == A[i - 1] && !visited[i - 1]))
+                continue;
+
+            if(last_number != -1 && !isSquare(A[i], last_number))
+                continue;
+
+            path.add(A[i]);
+            visited[i] = true;
+            helper(result, A, path, A[i], visited);
+            visited[i] = false;
+            path.remove(path.size() - 1);
+        }
+    }
+    
+    public boolean isSquare(int a, int b) {
+        return (Math.sqrt(a + b) % 1) == 0;
+    }
+}
