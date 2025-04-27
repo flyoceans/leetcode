8fefc2bcfb199b1afb06c1ba50f8421084466787
flyoceans <pengyq2009@gmail.com>
2025-04-26 22:06:53 -0700

    wiggle sort

---
+++ 324 Wiggle sort 2/Solution.java
@@ -0,0 +1,66 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        findKthSmallestNum(nums, nums.length/2, 0, nums.length-1);
+
+        int median = nums[nums.length/2];
+
+        for (int i = 0, j = 0, k = nums.length-1; j <= k; ) {
+            if (nums[j] > median) {
+                swap(nums, j, k);
+                k--;
+            } else if (nums[j] < median) {
+                swap(nums, i, j);
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        int[] copy = Arrays.copyOf(nums, nums.length);
+   
+        // int j = 0;
+        // for (int i = 0; i < nums.length; i += 2) {
+        //     nums[i] = copy[j++];
+        // }
+
+        // for (int i = 1; i < nums.length; i += 2) {
+        //     nums[i] = copy[j++];
+        // }
+
+    int i = nums.length - 1;
+    for (int j = 1; i >= nums.length/2 && j < nums.length; i--, j += 2) nums[j] = copy[i];
+    for (int j = 0; i >= 0; i--, j += 2) nums[j] = copy[i];
+    }
+
+    private void findKthSmallestNum(int[] nums, int k, int left, int right) {
+        if (left >= right) return;
+
+        int p = partition(nums, k, left, right);
+        if (p == k) return;
+        else if (p < k) findKthSmallestNum(nums, k, p+1, right);
+        else findKthSmallestNum(nums, k, left, p-1);
+    }
+
+    private int partition(int[] nums, int k, int left, int right) {
+        int pIndex = left + (right - left)/2;
+        int pivot = nums[pIndex];
+        swap(nums, pIndex, right);
+
+        int l = left, r = right-1;
+        while (l <= r) {
+            while (l <= r && nums[l] <= pivot) l++;
+            while (l <= r && nums[r] > pivot) r--;
+            if (l < r)
+                swap(nums, l, r);
+        }
+        swap(nums, right, l);
+        return l;
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+}
