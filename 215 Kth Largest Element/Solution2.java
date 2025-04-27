f7be5fdfbdd9ed47f2b3fa3594fc74adb140df4a
flyoceans <pengyq2009@gmail.com>
2025-04-26 18:35:24 -0700

    sort algo

--- 215 Kth Largest Element/Solution2.java
+++ 215 Kth Largest Element/Solution2.java
@@ -1,30 +1,37 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        quickSelect(nums, nums.length - k, 0, nums.length-1);
-        return nums[nums.length - k];
+        k = nums.length - k;
+
+        quickSelect(nums, k, 0, nums.length-1);
+        // System.out.println(Arrays.toString(nums));
+        return nums[k];
     }
     
     private void quickSelect(int[] nums, int k, int left, int right) {
         if (left >= right) return;
         
-        int mid = partition(nums, left, right);
-        if (mid == k) return;
-        else if (mid < k) quickSelect(nums, k, mid+1, right);
-        else quickSelect(nums, k, left, right - 1);
+        int p = partition(nums, left, right); // return index of pivot
+        if (p == k) return;
+        else if (p < k) quickSelect(nums, k, p+1, right);
+        else quickSelect(nums, k, left, p);
     }
     
     int partition(int[] nums, int left, int right) {
-        int pivot = nums[right];
-        
-        int l = left, r = right - 1;
-        while (l <= r) {
-            while (l <= r && nums[l] <= pivot) l++;
-            while (l <= r && nums[r] > pivot) r--;
+        int pivotIndex = (left + right)/2;
+        // int pivotIndex = new Random().nextInt(nums.size());
+        int pivot = nums[pivotIndex];
+        swap(nums, pivotIndex, right);
+
+        int l = left, r = right-1;
+        while (l < r) {
+            while (nums[l] <= pivot && l < r) l++;
+            while (nums[r] > pivot && l < r) r--;
             if (l < r)
                 swap(nums, l, r);
         }
-        swap(nums, right, l);
-        return l;
+        
+        swap(nums, right, r);
+        return r;
     }
     
     void swap(int[] nums, int a, int b) {
