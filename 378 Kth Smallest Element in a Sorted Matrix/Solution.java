0b7efb010ffb5ae6547feaa9cee27a7cc20c5111
flyoceans <pengyq2009@gmail.com>
2025-05-04 16:42:26 -0700

    binary search

--- 378 Kth Smallest Element in a Sorted Matrix/Solution.java
+++ 378 Kth Smallest Element in a Sorted Matrix/Solution.java
@@ -1,29 +1,36 @@
 class Solution {
-    public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        
-        int l = matrix[0][0], r = matrix[n-1][n-1];
+    public int splitArray(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++)
+            sum += nums[i];
+
+        int l = 0, r = sum;
+
         while (l + 1 < r) {
-            int mid = (l + r) /2;
-            int cnt = countLessOrEqual(matrix, mid); // O(M + N);
-            
-            if (cnt < k) {
+            int mid = l + (r - l)/2;
+            if (cal(nums, mid) > k) {
                 l = mid;
             } else {
                 r = mid;
             }
         }
 
-        if (countLessOrEqual(matrix, l) < k) return r;
-        return l;
+        if (cal(nums, l) == k) return l;
+        return r;
     }
-    
-    int countLessOrEqual(int[][] matrix, int x) {
-        int cnt = 0, c = matrix.length - 1; // start with the rightmost column
-        for (int r = 0; r < matrix.length; r++) {
-            while (c >= 0 && matrix[r][c] > x) --c;  // decrease column until matrix[r][c] <= x
-            cnt += (c + 1);
+
+    public int cal(int[] nums, int sum) {
+        int res = 1, curSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > sum) 
+                return Integer.MAX_VALUE;
+            if (curSum + nums[i] > sum) {
+                curSum = 0;
+                res++;
+            }
+            curSum += nums[i];
         }
-        return cnt;
+        if (curSum > sum) res++;
+        return res;
     }
 }
