45538f3b867eb944f11a292be464e37b602024f1
flyoceans <pengyq2009@gmail.com>
2025-05-18 17:30:33 -0700

    topo sorting

--- 207 Course Schedule/Solution.java
+++ 207 Course Schedule/Solution.java
@@ -1,34 +1,70 @@
 class Solution {
+    
+    enum State {
+        VISITED,
+        VISITING,
+        UNKNOWN;
+    }
+    
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>();
-        int[] status = new int[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<Integer>());
-        }
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        
         for (int[] pre : prerequisites) {
-            graph.get(pre[1]).add(pre[0]);
+            graph.computeIfAbsent(pre[0], k -> new ArrayList<>()).add(pre[1]);
         }
         
+        State[] states = new State[numCourses];
+        Arrays.fill(states, State.UNKNOWN);
         for (int i = 0; i < numCourses; i++) {
-            if (!dfs(graph, i, status)) return false;
+            if (dfs(states, i, graph) == State.VISITING) return false;
         }
         return true;
     }
     
-    private boolean dfs(List<List<Integer>> graph, int node, int[] status) {
-        if (status[node] == 1) //visited
-            return true;
-        if (status[node] == 2) //visiting, 因为graph里拓扑排序不能有环
-            return false;
+    State dfs(State[] states, int node, Map<Integer, List<Integer>> graph) {
+        if (states[node] != State.UNKNOWN) return states[node];
         
-        status[node] = 2;
-        List<Integer> neighbors = graph.get(node);
-        for (int i = 0; i < neighbors.size(); i++) {
-            if (!dfs(graph, neighbors.get(i), status))
-                return false;
+        states[node] = State.VISITING;
+        if (graph.containsKey(node)) {
+            for (int next : graph.get(node)) {
+                if (dfs(states, next, graph) == State.VISITING) return State.VISITING;
+            }
         }
-        status[node] = 1;
-        
-        return true;
+
+        states[node] = State.VISITED;
+        return State.VISITED;
     }
+
+    // public boolean canFinish(int numCourses, int[][] prerequisites) {
+    //     List<List<Integer>> graph = new ArrayList<>();
+    //     int[] status = new int[numCourses];
+    //     for (int i = 0; i < numCourses; i++) {
+    //         graph.add(new ArrayList<Integer>());
+    //     }
+    //     for (int[] pre : prerequisites) {
+    //         graph.get(pre[1]).add(pre[0]);
+    //     }
+        
+    //     for (int i = 0; i < numCourses; i++) {
+    //         if (!dfs(graph, i, status)) return false;
+    //     }
+    //     return true;
+    // }
+    
+    // private boolean dfs(List<List<Integer>> graph, int node, int[] status) {
+    //     if (status[node] == 1) //visited
+    //         return true;
+    //     if (status[node] == 2) //visiting, 因为graph里拓扑排序不能有环
+    //         return false;
+        
+    //     status[node] = 2;
+    //     List<Integer> neighbors = graph.get(node);
+    //     for (int i = 0; i < neighbors.size(); i++) {
+    //         if (!dfs(graph, neighbors.get(i), status))
+    //             return false;
+    //     }
+    //     status[node] = 1;
+        
+    //     return true;
+    // }
 }
